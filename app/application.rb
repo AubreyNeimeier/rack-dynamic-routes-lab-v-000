8a1cbd2908d40697987910870d8e196acb5741d4
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-14 16:40:54 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,21 +4,18 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
-
-    if req.path=="/items"
+    
       item = req.path.split("/items/").last
       item_obj = @@items.find {|x| x.name == item}
+      
       if @@items.include? (item_obj)
         resp.write "#{item_obj.price}"
-      else
+      elsif @@items.!include? (item_obj)
         resp.write "Item not found"
         resp.status = 400
-      end
-
-    else
-      resp.write "Route not found"
-      resp.status = 404
+      else
+        resp.write "Route not found"
+        resp.status = 404
     end
 
     resp.finish
